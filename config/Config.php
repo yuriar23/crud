c7e37fef73f62dfc338cefbaa0af8b955120335f
yuriar23 <yuriar1234567890@gmail.com>
2023-11-20 14:37:44 -0600

    mvc

--- config/Config.php
+++ config/Config.php
@@ -2,7 +2,7 @@
 define('BASE_URL', 'http://localhost//'); //En este caso, puede asignar el nombre que desee al proyecto.
 define('HOST', 'localhost');//Puede optar por dejarlo con el nombre por defecto
 define('USER', 'root');//Puede optar por dejarlo con el nombre por defecto."
-define('PASS', '');//dejar asi 
+define('PASS', '');//dejarlo asi
 define('DBNAME', '');//aqui va el nombre de la Bd
 define('CHARSET', 'charset=utf8');//Por favor, no realicen modificaciones
 ?>
